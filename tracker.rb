47c22d0cee7bf900adda199f9fcb9160740fce0e
mskalisz <mskalisz3@gmail.com>
2022-07-11 12:33:52 -0400

    Setup persistence files

--- tracker.rb
+++ tracker.rb
@@ -2,6 +2,21 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+require_relative "database_persistence"
+require_relative "session_persistence"
+
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+  set :erb, :escape_html => true
+end
+
+configure(:development) do
+  require "sinatra/reloader"
+  also_reload "database_persistence.rb"
+  also_reload "session_persistence.rb"
+end
+
 get "/" do
   redirect "/lists"
 end
