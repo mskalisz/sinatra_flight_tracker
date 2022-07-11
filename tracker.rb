f7565420bd208cbcc85d124f3169156876f08efe
mskalisz <mskalisz3@gmail.com>
2022-07-11 11:30:05 -0400

    Display flight name and date and fix styling

--- tracker.rb
+++ tracker.rb
@@ -3,5 +3,13 @@ require "sinatra/reloader"
 require "tilt/erubis"
 
 get "/" do
-  erb "You have no flights.", layout: :layout
+  redirect "/lists"
+end
+
+get "/lists" do
+  @flights = [
+    {name: "Hawaii", date: ["6.12.2022"]},
+    {name: "Ireland", date: ["9.14.2022"]}
+  ]
+  erb :flights, layout: :layout
 end
