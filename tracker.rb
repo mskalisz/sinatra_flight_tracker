9fc17ba9dd930de3b0742f2ad7c30434392609e2
mskalisz <mskalisz3@gmail.com>
2022-07-15 18:45:38 -0400

    Begin compiling errors, need to fix compile_error method - make sure variable passed in is translating correctly

--- tracker.rb
+++ tracker.rb
@@ -36,6 +36,25 @@ get "/flights/new" do
   erb :new_trip, layout: :layout
 end
 
+# Return an error message if the name is invalid, otherwise return nil if the name is valid.
+def compile_errors(field_to_check)
+  errors = []
+  p field_to_check
+  if field_to_check == "flight_number"
+    if !(1..6).cover? field_to_check.size
+      errors << "This field must be between 1 and 6 characters."
+    end
+  else
+    if !(1..50).cover? field_to_check.size
+      errors << "This field must be between 1 and 50 characters."
+    elsif session[:flights].any? { |flight| flight[:name] == field_to_check }
+      errors << "The trip name must be unique."
+    end
+  end
+  return errors unless errors.empty?
+  nil
+end
+
 # Create a new trip
 post "/flights" do
   trip_name = params[:trip_name].strip
@@ -44,15 +63,18 @@ post "/flights" do
   flight_number = params[:flight_number].strip
   flight_date = params[:flight_date].to_s
 
-  if !(1..50).cover? trip_name.size
-    session[:error] = "This field must be between 1 and 50 characters."
-    erb :new_trip, layout: :layout
-  elsif session[:flights].any? { |flight| flight[:name] == trip_name }
-    session[:error] = "The trip name must be unique."
-    erb :new_trip, layout: :layout
-  else
-    session[:flights] << {name: trip_name, destination: destination_name, date: flight_date}
-    session[:success] = "Your trip has been created!"
-    redirect "/flights"
-  end
+  params_to_check = [trip_name, destination_name, airline, flight_number]
+
+  # params_to_check.each do |name|
+    if errors = compile_errors(flight_number)
+      errors.each do |error|
+        session[:error] = error
+      end
+      erb :new_trip, layout: :layout
+    else
+      session[:flights] << {name: trip_name, destination: destination_name, date: flight_date}
+      session[:success] = "Your trip has been created!"
+      redirect "/flights"
+    end
+  # end
 end
